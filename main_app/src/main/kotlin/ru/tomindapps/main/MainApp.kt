8b4fb83bacee8c8be5cda853ce75424ece026e9d
Ivan Yushenkov <IVseYushenkov@sberbank.ru>
2023-03-16 11:51:35 +0300

    made bot more friendly

--- main_app/src/main/kotlin/ru/tomindapps/main/MainApp.kt
+++ main_app/src/main/kotlin/ru/tomindapps/main/MainApp.kt
@@ -3,34 +3,76 @@ package ru.tomindapps.main
 import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
 import com.github.kotlintelegrambot.dispatcher.text
 import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.ParseMode
+import com.github.kotlintelegrambot.entities.User
 import java.util.regex.Pattern
 
 class MainApp {
+
     fun start() {
         val bot = bot {
             token = "5821512388:AAGtX3TNAwu7QuN_pc9hx915IgILAW8soVc"
             dispatch {
+                command("start") {
+                    bot.send(message.from.getGreetingMessage(), update.message!!.chat.id)
+                }
                 text {
-                    text
+                    val result = text
+                        .log(message.from?.username.orEmpty())
                         .checkPhone {
-                            (ERROR + it).send(bot, message.chat.id)
+                            bot.send(message.from.getErrorMessage(it), message.chat.id)
                         }
                         .clean()
                         .addToUrl()
-                        .send(bot, message.chat.id)
+
+                    if (result.isNotEmpty()) {
+                        bot.send(message.from.getFriendMessage(), message.chat.id)
+                        bot.send(result, message.chat.id)
+                    }
                 }
             }
         }
         bot.startPolling()
     }
 
-    private fun String.send(bot: Bot, id: Long) {
-        if (this.isNotEmpty()) bot.sendMessage(ChatId.fromId(id), text = this)
+    private fun User?.getGreetingMessage(): String {
+        return if (this?.languageCode?.contains("ru", true) == true) {
+            GREETING_RU
+        } else {
+            GREETING_EN
+        }.trimIndent()
+    }
+
+    private fun User?.getErrorMessage(text: String): String {
+        return if (this?.languageCode?.contains("ru", true) == true) {
+            ERROR_RU_1 + text + ERROR_RU_2
+        } else {
+            ERROR_EN_1 + text + ERROR_EN_2
+        }.trimIndent()
+    }
+
+    private fun User?.getFriendMessage(): String {
+        return if (this?.languageCode?.contains("ru", true) == true) {
+            FRIEND_RU
+        } else {
+            FRIEND_EN
+        }.trimIndent()
+    }
+
+    private fun String.log(name: String): String {
+        if (this.isNotEmpty()) println("message $this from $name")
+        return this
+    }
+
+    private fun Bot.send(text: String, id: Long) {
+        if (text.isNotEmpty()) this.sendMessage(ChatId.fromId(id), text = text, parseMode = ParseMode.MARKDOWN)
     }
 
     private fun String.checkPhone(onFailure: (String) -> Unit): String {
+        if (this.contains("start")) return Strings.Empty
         return this.phoneValidator {
             onFailure(this)
             Strings.Empty
@@ -38,11 +80,16 @@ class MainApp {
     }
 
     private fun String.clean(): String {
-        return if (this.isNotEmpty()) {
-            removePrefix("+")
+        var result = if (this.isNotEmpty()) {
+            this.removePrefix("+")
         } else {
             this
         }
+
+        if (result.length == 11 && result.startsWith('8')) {
+            result = result.replaceFirst('8', '7')
+        }
+        return result
     }
 
     private fun String.addToUrl(): String {
@@ -54,7 +101,24 @@ class MainApp {
     }
 
     private companion object {
-        private const val ERROR = "Unable to determine the correct phone number - "
+        private const val FRIEND_RU = "Вот твоя прямая ссылка, друг!"
+        private const val FRIEND_EN = "Here's a direct link, friend!"
+        private const val ERROR_RU_1 = "Не удалось правильно определить номер телефона - \"*"
+        private const val ERROR_RU_2 = "*\", попробуй ещё раз:)"
+        private const val ERROR_EN_1 = "Unable to determine the correct phone number - \"*"
+        private const val ERROR_EN_2 = "*\", try it again please:) "
+        private const val GREETING_RU = """
+            Привет! Я чат-бот и помогу тебе открыть чат WhatsApp не сохраня контакт в книгу контактов! 
+            Просто отправь мне номер в международном формате или начиная с "8"!
+            Например +79991114455 или 79991114455 или 89991114455!
+            К сожалению я понимаю только цифры, поэтому могу ответить тебе ошибкой!:)
+        """
+        private const val GREETING_EN = """
+            Hi! I'm a chatbot and will help you open a WhatsApp chat without keeping a contact in your contact book! 
+            Just send me a number in international format or starting with "8"!
+            For example +79991114455 or 79991114455 or 89991114455!
+            Unfortunately, I only understand numbers, so I can answer you by mistake:)
+        """
     }
 
 }
